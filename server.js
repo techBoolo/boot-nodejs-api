adce266253b90da52db7da04206d3c40bad40aee
teklu <teklufa@gmail.com>
2023-06-02 10:20:10 +0300

    use express to handle request
    install and use express to handle requests in our app and pass the handler to our server
    use nodemon in our script to run our server during development

--- server.js
+++ server.js
@@ -1,11 +1,10 @@
 import http from 'node:http'
+import app from './app.js'
 
 const server = http.createServer()
 const PORT = process.env.PORT || 3001
 
-server.on('request', (req, res) => {
-  res.end('request handled')
-})
+server.on('request', app)
 
 server.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
